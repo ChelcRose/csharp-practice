22275206044d599f762aeba8365af0e430cc2f98
ChelcRose <143089725+ChelcRose@users.noreply.github.com>
2026-09-05 01:07:23 +0800

    practice: add C# fundamentals exercises

---
+++ CSharpFundamentals/FirstCode.cs
@@ -0,0 +1,23 @@
+//Console.WriteLine("Hi, i'm Chelsea");
+
+//Console.WriteLine("Congratulations!");
+//Console.Write("You wrote your first lines of code.");
+
+//Console.WriteLine("This is the first line.");
+//Console.WriteLine("This is the second line.");
+
+// Microsoft Learn: Store and Retrieve Data Using Literal and Variable Values in C#
+// char
+//Console.WriteLine('b');
+
+// int
+//Console.WriteLine(123);
+
+// float
+//Console.WriteLine(12.5F);
+
+// double
+//Console.WriteLine(12.5);
+
+// decimal
+//Console.WriteLine(10.232m);
---
+++ CSharpFundamentals/VariablesPractice.cs
@@ -0,0 +1,22 @@
+public class VariablesPractice
+{
+    public static void Run()
+    {
+        string name = "Bob";
+        int messages = 3;
+        decimal temp = 34.4m;
+
+        Console.Write("Hello, ");
+        Console.Write(name);
+        Console.Write("! You have ");
+        Console.Write(messages);
+        Console.Write(" messages in your inbox. ");
+        Console.Write("The temperature is ");
+        Console.Write(temp);
+        Console.Write(" celsius.");
+
+        
+    }
+
+
+}
